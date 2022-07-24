957b2766acf954cbef93ff81dac6ea17347d1714
kishor.sarkar <kishor81160@gmail.com>
2022-07-24 09:48:00 +0530

    states

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 import Greet from "./components/Creet";
 import Welcome from "./components/welcome";
 import Hllow from "./components/hllow";
+import Messagez from "./components/message";
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Greet name = "Bruce" heroName="Batman">
+      {/* <Greet name = "Bruce" heroName="Batman">
         <p>This is children Propertics</p>
       </Greet>
       <Greet name = "Diana" heroName ="Wander Women"/>
@@ -16,7 +17,8 @@ class App extends Component {
       <Greet name = "Viswas" heroName= "Tutor"/>
       <Welcome  name = "Bruce" heroName="Batman" />
       <Welcome name = "Diana" heroName ="Wander Women"/>
-      <Welcome name = "Kishor" heroName = "Begineer"/>
+      <Welcome name = "Kishor" heroName = "Begineer"/> */}
+      <Messagez />
 {/* 
       <Welcome />
         
