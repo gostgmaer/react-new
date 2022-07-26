6ef9cf5bd4af812b8d882f212f0988efb3fb538b
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-07-26 21:37:28 +0530

    complte form

--- my-app/src/components/form.js
+++ my-app/src/components/form.js
@@ -1,41 +1,65 @@
 import React, { Component } from 'react'
 
- class form extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         userName:"",
-         commentss:""
-      }
-    }
-    handleUsernameChange = (event) =>{
-        this.setState({
-            userName:event.target.value
-        })
+class form extends Component {
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      userName: "",
+      commentss: "",
+      Topic: "react"
     }
-    handleCommentChange = (event) =>{
-        this.setState({
-            commentss:event.target.value
-        })
-    }
+  }
+  handleUsernameChange = (event) => {
+    this.setState({
+      userName: event.target.value
+    })
+
+  }
+  handleCommentChange = (event) => {
+    this.setState({
+      commentss: event.target.value
+    })
+  }
+  handleTopicChange = (event) => {
+    this.setState({
+      Topic: event.target.value
+    })
+  }
+  handleSubmit = event=>{
+    alert(`${this.state.userName},${this.state.commentss},${this.state.Topic}`)
+    event.preventDefault()
+  }
 
 
   render() {
+    const {userName,commentss,Topic}=this.state
     return (
-        <div>
-        <div className="form-group">
-          <label>UserName</label>
-          <input onChange={this.handleUsernameChange} type="text"
-            className="form-control" name="username" value={this.state.userName} id="username" aria-describedby="helpId" placeholder="UserName"/>
-          
-        </div>
-        <div className="form-group">
-          <label>Comment</label>
-          <textarea className="form-control" rows="3" value={this.state.comments} onChange={this.handleCommentChange}></textarea>
-        </div>
-    </div>
+
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div >
+            <label>UserName</label>
+            <input onChange={this.handleUsernameChange} type="text"
+              className="form-control" name="username" value={userName} id="username" aria-describedby="helpId" placeholder="UserName" />
+
+          </div>
+          <div >
+            <label>Comment</label>
+            <textarea className="form-control" rows="3" value={commentss} onChange={this.handleCommentChange}></textarea>
+          </div>
+          <div >
+            <label >Select</label>
+            <select value={Topic} name="" id="" onChange={this.handleTopicChange}>
+              <option value="react">Option 1</option>
+              <option value="angular">Option 2</option>
+              <option value="vue"> Option 3</option>
+            </select>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+
+      </div>
     )
   }
 }
